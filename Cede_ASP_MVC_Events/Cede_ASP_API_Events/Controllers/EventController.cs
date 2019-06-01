ec378a0e26a39a72a77de14ba4c79463c9f24358
Miguel teheran <miguelteheran@gmail.com>
2019-06-01 09:11:29 -0500

    addin event controller to API and the service to Event.Main

---
+++ Cede_ASP_MVC_Events/Cede_ASP_API_Events/Controllers/EventController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Cede_ASP_API_Events_EF.Context;
+using Cede_ASP_API_Events_EF.Entities;
+
+namespace Cede_ASP_API_Events.Controllers
+{
+    public class EventController : ApiController
+    {
+        private EventterContext db = new EventterContext();
+
+        // GET: api/Event
+        public IQueryable<Event> GetEvents()
+        {
+            return db.Events;
+        }
+
+        // GET: api/Event/5
+        [ResponseType(typeof(Event))]
+        public async Task<IHttpActionResult> GetEvent(Guid id)
+        {
+            Event objevent = await db.Events.FindAsync(id);
+            if (objevent == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(objevent);
+        }
+
+        // PUT: api/Event/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutEvent(Guid id, Event objevent)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != objevent.EventId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(objevent).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EventExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Event
+        [ResponseType(typeof(Event))]
+        public async Task<IHttpActionResult> PostEvent(Event objevent)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Events.Add(objevent);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (EventExists(objevent.EventId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = objevent.EventId }, objevent);
+        }
+
+        // DELETE: api/Event/5
+        [ResponseType(typeof(Event))]
+        public async Task<IHttpActionResult> DeleteEvent(Guid id)
+        {
+            Event objevent = await db.Events.FindAsync(id);
+            if (objevent == null)
+            {
+                return NotFound();
+            }
+
+            db.Events.Remove(objevent);
+            await db.SaveChangesAsync();
+
+            return Ok(objevent);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EventExists(Guid id)
+        {
+            return db.Events.Count(e => e.EventId == id) > 0;
+        }
+    }
+}
---
+++ Cede_ASP_MVC_Events/Cede_ASP_MVC_Events_Main/Services/EventService.cs
@@ -0,0 +1,32 @@
+﻿using Cede_ASP_MVC_Events_Main.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Cede_ASP_MVC_Events_Main.Services
+{
+    public class EventService
+    {
+        const string ApiBase = "http://localhost:60469/api/";
+
+        public HttpClient httpClient { get; set; } = new HttpClient();
+
+        public async Task<List<Event>> GetPersonals()
+        {
+            var result = await httpClient.GetAsync($"{ApiBase}/event");
+
+            if (result.IsSuccessStatusCode)
+            {
+                var parseResult = JsonConvert.DeserializeObject<List<Event>>(await result.Content.ReadAsStringAsync());
+
+                return parseResult;
+            }
+
+            return new List<Event>();
+        }
+    }
+}
