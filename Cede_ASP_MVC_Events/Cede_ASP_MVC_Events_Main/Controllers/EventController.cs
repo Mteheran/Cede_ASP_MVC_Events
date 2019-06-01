58edd4d7c0538738ec932d43cbc4cbddcb98d1c1
Yesid Delgado Plata <ydelgadoplata@outlook.com>
2019-06-01 09:51:33 -0500

    ydelgado

---
+++ Cede_ASP_MVC_Events/Cede_ASP_MVC_Events_Main/Controllers/EventController.cs
@@ -0,0 +1,29 @@
+﻿using Cede_ASP_MVC_Events_Main.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cede_ASP_MVC_Events_Main.Controllers
+{
+    public class EventController : Controller
+    {
+        
+        public EventService eventService { get; set; }
+
+        public EventController()
+        {
+            eventService = new EventService();
+        }
+        
+        // GET: Event
+        public async Task<ActionResult> Events()
+        {
+            var eventList = await eventService.GetPersonals();
+
+            return View(eventList);
+        }
+    }
+}
