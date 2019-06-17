eb447f3302face89f09ca59b4cb4e696cb8db899
Yesid Delgado Plata <ydelgadoplata@gmail.com>
2019-06-17 13:36:16 -0500

    Agregado Helper de control "IsPrivate" A espera de sincronía con repo de mteheran

---
+++ Cede_ASP_MVC_Events/Cede_ASP_MVC_Events_Main/Helpers/IsPrivateHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cede_ASP_MVC_Events_Main.Helpers
+{
+    public static class IsPrivateHelper
+    {
+        public static string GetStatus(bool IsPrivate)
+        {
+            switch (IsPrivate)
+            {
+                case true:
+                    return "Privado";
+                case false:
+                    return "Público";
+                default:
+                    break;
+            }
+
+            return "";
+        }
+    }
+}
